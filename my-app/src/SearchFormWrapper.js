29518ea7a81aba0f87b71426f35a9c554df2fa93
Juan Plazas <juan_plazas@epam.com>
2026-01-19 13:39:42 -0500

    UPDATE | added possibility to add and edit

--- my-app/src/SearchFormWrapper.js
+++ my-app/src/SearchFormWrapper.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import SearchForm from "./SearchForm";
 
 export default function SearchFormWrapper() {
-  return React.createElement(SearchForm, {
-    initialQuery: "", // optional default
-    onSearch: (value) => console.log("Search triggered:", value)
-  });
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(SearchForm, {
+      initialQuery: "", // optional default
+      onSearch: function (value) {
+        console.log("Search triggered:", value);
+      }
+    }),
+    React.createElement(Outlet) // 👈 REQUIRED for /new
+  );
 }
--- my-app/src/router.js
+++ my-app/src/router.js
@@ -1,18 +1,26 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MovieListPage from "./MovieListPage";
-import SearchFormWrapper from "./SearchFormWrapper";
-import MovieDetailsWrapper from "./MovieDetailsWrapper";
+import EditMovieForm from "./EditMovieForm";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: React.createElement(MovieListPage),
     children: [
-      { path: "/", element: React.createElement(SearchFormWrapper) },
-      { path: "/:movieId", element: React.createElement(MovieDetailsWrapper) }
+      {
+        index: true,
+        element: React.createElement(SearchFormWrapper)
+      },
+      {
+        path: "new",
+        element: React.createElement(AddMovieForm)
+      },
+      {
+        path: ":movieId/edit",
+        element: React.createElement(EditMovieForm)
+      },
+      {
+        path: ":movieId",
+        element: React.createElement(MovieDetailsWrapper)
+      }
     ]
   }
 ]);
-
-export default router;
